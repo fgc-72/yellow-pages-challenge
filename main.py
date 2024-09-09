485a7c2fc3d43d1832584f379497a0a923dcbbf0
zeebaz23 <sebastianaguiar462@gmail.com>
2024-09-09 09:53:07 -0500

    Avance clase 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Tu código aquí
+print("1 para ver la lista")
+print("2 para agregar un usuario")
+print("3 para modificar un usario")
+print("4 para eliminar un usario")
+print("5 para salir")
+
+opciones = int(input("Escoja la opción"))
+
+if opciones == 1:
+    print(people)
